07e304453aa8fae02a2636407feaae9e3dacab4e
Souloutz <67064694+Souloutz@users.noreply.github.com>
2022-01-15 23:01:52 -0500

    Add command & heroku worker

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const Discord = require("discord.js")
-require("dotenv").config()
-
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("message", (message) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+    const channelID = message.channel.id;
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(" ");
+    const command = args.shift().toLowerCase();
 })
 
 client.on("messageCreate", (message) => {
@@ -18,4 +27,10 @@ client.on("messageCreate", (message) => {
     }
 })
 
-client.login(process.env.TOKEN)
+const welcomeChannelID = "471693842018467853"
+
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(welcomeChannelID).send(`<@${member.id}> Welcome to the server!`)
+})
+
+client.login(token)
